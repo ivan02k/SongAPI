d2a0442e74099aa1aa6ea95a8da9773d5bb2a0a7
Ivan I <Ivan I@DESKTOP-F2FA450>
2023-01-06 21:51:55 +0200

    data

--- Data/SongContext.cs
+++ Data/SongContext.cs
@@ -14,7 +14,9 @@ namespace Data
     public class SongContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
     {
 
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         public SongContext(DbContextOptions<SongContext> options)
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
         : base(options)
         {
         }
